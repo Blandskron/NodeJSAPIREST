b72e94558ca0462358e54bf8ba404c8e57e08105
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-09-21 01:47:31 -0300

    Adding routes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('My App');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+  res.send("Hola mi server en Express");
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy la nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
+app.listen(port, () =>{
+  console.log("My port: " + port);
+});
