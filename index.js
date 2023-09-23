ec2c910f0bf4bc0e488a4f62a1e6503e1938cc20
Bastian Landskron <93203724+Blandskron@users.noreply.github.com>
2023-09-22 21:39:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
-<<<<<<< HEAD
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
-=======
->>>>>>> 99e6de2fd22eb0f5788d4966dbae5008bedead91
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -21,15 +31,11 @@ app.get('/nueva-ruta', (req, res) => {
 
 routerApi(app);
 
-<<<<<<< HEAD
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
-=======
-
->>>>>>> 99e6de2fd22eb0f5788d4966dbae5008bedead91
 app.listen(port, () => {
   console.log('Mi port' +  port);
 });
